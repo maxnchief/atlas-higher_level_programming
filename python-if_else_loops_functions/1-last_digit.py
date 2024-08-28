3c4d15fae6baadea9549fa6e83fa2c4fa78fc780
maxnchief <logandogan@gmail.com>
2024-08-28 16:49:38 +0000

    adding a check for less than 6

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,3 +4,5 @@ number = random.randint(-10000, 10000)
 s = str(number)
 last = int(s[-1])
 print(f"Last digit of {number} is {last}")
+if last <= 5 and =! 0
+    print(f"and is less than 6 and not 0")
