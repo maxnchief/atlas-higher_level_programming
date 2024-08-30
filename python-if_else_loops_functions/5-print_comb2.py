b4ab03c8f40f7bcac0db1e7b22201d6ac2362b99
maxnchief <logandogan@gmail.com>
2024-08-30 19:21:53 +0000

    so close i just have to remove the extra digit from single digit numbers

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 for n in range(100):
+    s = str(n)
     if n < 10:
-        single_digit = ("0" + n)
-   else:
-        continue
+        single_digit = ("0" + s)
+    elif n >= 10:
+        single_digit = ("")
+    else:
+        pass
     if n < 99:
          print("{1}{0}, ".format(n, single_digit), end="")
     else:
