8f2e0cfc0ef6da7c36bd1f7a44692b70a8b34857
maxnchief <logandogan@gmail.com>
2024-08-30 17:53:36 +0000

    fixed the if statement

--- python-if_else_loops_functions/3-print_alpahbt.py
+++ python-if_else_loops_functions/3-print_alpahbt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+n = 97
 while n < 123:
-    n += 1
-    if n == "q" or "e":
-     continue
-print("{}".format(chr(n)), end="")
+  n += 1
+  if not n == 99 or 120:
+    print("{}".format(chr(n)), end="")
+  else:
+    continue
