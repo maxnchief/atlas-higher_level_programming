acfd3af3b094fdcd641c910e8f740408b9a9de18
maxnchief <logandogan@gmail.com>
2024-08-30 19:06:20 +0000

    added another format to sepearate the added 0 digit and moved the second print to the 99 comma removal

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 for n in range(100):
     if n < 10:
-        print("0{}, ".format(n), end="")
+        single_digit = ("0" + n)
+   else:
+        continue
+    if n < 99:
+         print("{1}{0}, ".format(n, single_digit), end="")
     else:
-        print("{}, ".format(n), end="")
+        print("{} ".format(n), end="")
