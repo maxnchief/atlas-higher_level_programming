b7f2e12c01d12556d3b96283556f7c701c395612
maxnchief <logandogan@gmail.com>
2024-08-30 17:39:02 +0000

    fixed indentation

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-    while n < 123:
-        n += 1
-    if n == "q" or "e":
-        continue
-    print("{}".format(chr(n)), end="")
+while n < 123:
+    n += 1
+if n == "q" or "e":
+    continue
+print("{}".format(chr(n)), end="")
