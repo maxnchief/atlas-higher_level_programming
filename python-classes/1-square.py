e768adf2192906643d443957a48830ba45864c8e
maxnchief <logandogan@gmail.com>
2024-09-08 18:59:04 +0000

    propper documentaion?

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,13 +3,12 @@
 
 
 class Square:
-    
-    
     """we are inside the class"""
-    
-    
-    def __init__(self, size)
-    
-    
-    """this is the size instance"""
-    
+
+    def __init__(self, size):
+        """this is the instance of size
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
