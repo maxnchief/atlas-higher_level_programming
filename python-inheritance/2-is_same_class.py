aedc7cebdd5270a0497184ab286139f92cc6c5c4
maxnchief <logandogan@gmail.com>
2024-09-18 02:37:17 +0000

    returngin

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""defines class checking function"""
+
+def is_same_class(obj, a_class):
+    """checks if there is another instance of the same class"""
+
+if type(obj) == a_class
+    return True
+return False
