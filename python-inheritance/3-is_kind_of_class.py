2038771795150b30a26c0ab4f18a41ce88a6c32d
maxnchief <logandogan@gmail.com>
2024-09-18 02:46:33 +0000

    syntax

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,11 +2,10 @@
 """ module: 2-is_same_classs"""
 
 
-def is_kind_of_class(obj, a_class)
+def is_kind_of_class(obj, a_class):
     """funtion: is_kind_of_class
     obj" an object
     a_class: a class
     Returns: Bool"""
 
     return isinstance(obj, a_class)
-    
