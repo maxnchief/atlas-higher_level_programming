496244968ff407fc709fcef9a750da4c3177b7ee
maxnchief <logandogan@gmail.com>
2024-09-18 02:51:23 +0000

    frank ocean got me somber

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module: 4-inherts_from"""
+
+
+def inherts_from(obj, a_class):
+    """the object is an inherited class
+    obj: an object
+    a_class: a class
+    returns none"""
+
+    return type(obj) != a_class and isinstance(obj, a_class)
+    
