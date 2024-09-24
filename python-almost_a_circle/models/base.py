faa8b382db975039ee1896053ea68f102d1e884c
maxnchief <logandogan@gmail.com>
2024-09-24 19:57:41 +0000

    indents?

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,11 +8,11 @@ class Base:\
     ''' this is a mf base class and this is the mf documentaion gimmie point
         @id: The id for a specific instance
     '''
-    __nb_objects = 0
+__nb_objects = 0
 
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+def __init__(self, id=None):
+    if id is not None:
+        self.id = id
+    else:
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
