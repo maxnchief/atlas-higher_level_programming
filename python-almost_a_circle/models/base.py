f9f76cedb5bbd380e7b11831d8f3f075b08b40f0
maxnchief <logandogan@gmail.com>
2024-09-24 19:55:15 +0000

    main

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,6 @@ class Base:\
     ''' this is a mf base class and this is the mf documentaion gimmie point
         @id: The id for a specific instance
     '''
-
     __nb_objects = 0
 
     def __init__(self, id=None):
