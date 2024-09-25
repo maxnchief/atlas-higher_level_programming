9327b5a56eeb46b22ada835ce27e75d0454c12a0
maxnchief <logandogan@gmail.com>
2024-09-25 16:19:38 +0000

    two lines

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,14 +3,16 @@
 will open the file
 '''
 
+
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
         
         '''
         this function opens a file and reads it
         Args:
-            filename - the name of the file being read
+            @filename - the name of the file being read
         '''
+
 
     content = file.read()
     print(content, end='')
