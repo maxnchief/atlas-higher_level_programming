e0c121210fb1fc35021a195435865e861658ce06
maxnchief <logandogan@gmail.com>
2024-09-25 14:08:57 -0500

    removed content

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 def write_file(filename="", text=""):
     '''function will write content to utf8 file'''
     with open(filename, 'w', encoding='utf-8') as file:
-        content = file.write(filename)
+        file.write(filename)
         return len(filename)
