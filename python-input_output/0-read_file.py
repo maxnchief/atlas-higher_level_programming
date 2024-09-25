1846d1074b244c1ebc36d6f41a89435a8d56cec4
maxnchief <logandogan@gmail.com>
2024-09-25 15:52:25 +0000

    documentainon

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''
+function will open the file
 
+'''
 def read_file(filename=""):
+    '''
+    this function opens a file and reads it
+    '''
+    
     with open('UTF8'):
