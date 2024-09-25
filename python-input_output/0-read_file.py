606a11c290cd7a7e2bf5cb9df7c826f026def2ab
maxnchief <logandogan@gmail.com>
2024-09-25 15:53:28 +0000

    dont know the spacing

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''
 function will open the file
-
 '''
+
 def read_file(filename=""):
+    
     '''
     this function opens a file and reads it
     '''
-    
-    with open('UTF8'):
+
+    with open('UTF8')
