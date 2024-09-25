662739f17310cadfd8215dcab37b6b56d3f360a2
maxnchief <logandogan@gmail.com>
2024-09-25 16:31:35 +0000

    writting the file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' this file will write content to file'''
+
+def write_file(filename="", text="")
+    '''function will write content to utf8 file'''
+    with open(filename, 'w', encoding='utf-8') as file:
+        content = file.write()
+        return len(content)
