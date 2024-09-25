9961ab25b9f023200846d354be3219397fa5ada9
maxnchief <logandogan@gmail.com>
2024-09-25 16:33:04 +0000

    syntax

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' this file will write content to file'''
 
-def write_file(filename="", text="")
+def write_file(filename="", text=""):
     '''function will write content to utf8 file'''
     with open(filename, 'w', encoding='utf-8') as file:
         content = file.write()
