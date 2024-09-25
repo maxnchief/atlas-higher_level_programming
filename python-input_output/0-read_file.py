8ac111f3f58638aa1a105c33cc18ac23fc2548d2
maxnchief <logandogan@gmail.com>
2024-09-25 16:25:31 +0000

    indent

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     '''prints the contents of the utf-8 file to stdout'''
     with open(filename, 'r', encoding='utf-8') as file:
-    content = file.read()
-    print(content, end='')
+        content = file.read()
+        print(content, end='')
     
