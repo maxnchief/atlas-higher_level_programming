9327c9f172d26cc17927df770a4207fdc5574cbf
maxnchief <logandogan@gmail.com>
2024-09-25 16:20:47 +0000

    doubler lineser

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,7 @@ will open the file
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
         
+      
         '''
         this function opens a file and reads it
         Args:
