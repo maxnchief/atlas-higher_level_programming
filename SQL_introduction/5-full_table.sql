e2cabe4b1335512fe379904340fec8ed0aff7288
maxnchief <logandogan@gmail.com>
2024-10-13 14:08:06 -0500

    easier than tho

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,15 +1,4 @@
 -- desrcribe without describe
 USE $1;
 
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = '$1' 
-    AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table
