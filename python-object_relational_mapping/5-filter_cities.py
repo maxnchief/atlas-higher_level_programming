e28adc22abda855d91eeb23adf030f1136d9562e
maxnchief <logandogan@gmail.com>
2024-10-15 15:18:10 -0500

    Refactor SQL query to filter cities by state and sort by id

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,38 +1,19 @@
 #!/usr/bin/python3
-
-"""takes in the name of a state as an argument and lists all cities of that state"""
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <mysql username> <mysql password> <database name> <state name>")
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
-    cursor = db.cursor()
-
-    # Execute the query
-    query = """
-    SELECT cities.id, cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
-
-    # Fetch and print the results
-    cities = cursor.fetchall()
-    print(", ".join([city[1] for city in cities]))
-
-    # Close the cursor and database connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
     db.close()
