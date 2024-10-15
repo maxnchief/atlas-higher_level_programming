35dda50069f8ee527272cefd26ec54dcdcad29ec
maxnchief <logandogan@gmail.com>
2024-10-15 15:09:02 -0500

    reordered

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+"""takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+
 import MySQLdb
 import sys
 
-"""takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+
 
 if __name__ == "__main__":
     # Get the arguments
