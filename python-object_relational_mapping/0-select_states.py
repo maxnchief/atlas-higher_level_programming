de0adac38db29a4ff6d0569da1eac50741c423e3
maxnchief <logandogan@gmail.com>
2024-10-15 20:50:35 -0500

    Refactor script to fetch and print all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
-
-"""this module lists all states from the database hbtn_0e_0_usa"""
-
-import MySQLdb
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Execute the SQL query to select all states sorted by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows from the executed query
-    rows = cursor.fetchall()
-
-    # Print the rows
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
