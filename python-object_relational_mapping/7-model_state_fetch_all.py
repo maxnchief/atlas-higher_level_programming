2ab2d16327c2b89efdfdb248355ee4b68f3d3741
maxnchief <logandogan@gmail.com>
2024-10-15 20:39:39 -0500

    Refactor script to fetch and print all states from the database

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,35 +1,22 @@
-import sys
+#!/usr/bin/python3
+"""
+All states via SQLAlchemy
+"""
+
+from sys import argv
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-#!/usr/bin/python3
-"""
-Lists all State objects from the database hbtn_0e_6_usa
-"""
-
-
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    # Create an engine to the MySQL database
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}')
-
-    # Create a configured "Session" class
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                        argv[2],
+                                                        argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
-
-    # Query all State objects and order by id
-    states = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
     session.close()
