396bdd4e251c47297133c5a49364669a0579c1df
maxnchief <logandogan@gmail.com>
2024-11-22 13:10:46 -0600

    add script to count appearances of a character in Star Wars films

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+
+
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+        film.characters.forEach(character => {
+            if (character.includes(`/${characterId}/`)) {
+                count++;
+            }
+        });
+    });
+
+    console.log(count);
+});
